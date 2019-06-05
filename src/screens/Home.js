889c85bdbae74e93757a0338657b7af4fcfa5519
ItsMeAndrea <itsandrea_94@hotmail.com>
2019-06-05 08:59:51 -0400

    home update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,30 +1,132 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { Button } from 'native-base';
+import Swipeable from 'react-native-swipeable';
+
+const rightButtons = [
+  <Button
+    style={{
+      width: 400,
+      height: 100,
+      backgroundColor: '#279e29',
+      padding: 25
+    }}
+  >
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('../images/edit.png')}
+    />
+  </Button>,
+  <Button
+    style={{ width: 400, height: 100, backgroundColor: '#bc2121', padding: 25 }}
+  >
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('../images/cancel.png')}
+    />
+  </Button>
+];
 
 export default class Home extends Component {
   static navigationOptions = {
-    title: 'Reservación',
+    title: 'Tus Reservaciones',
     headerStyle: {
       backgroundColor: '#282828'
     },
     headerTitleStyle: {
-      color: 'white'
+      color: 'white',
+      textAlign: 'center',
+      flex: 1
     }
   };
+
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: '#282828' }}>
-        <Text style={{ color: 'white' }}>HELLO WORLD</Text>
+        <Swipeable
+          leftButtonWidth={100}
+          rightButtonWidth={100}
+          rightButtons={rightButtons}
+          style={{
+            height: 100,
+            backgroundColor: 'gray',
+            
+          }}
+        >
+          <Text
+            style={{
+              color: 'white',
+              fontWeight: 'bold',
+              fontSize: 20,
+              paddingLeft: 20,
+              paddingTop: 10
+            }}
+          >
+            Mariella Duran
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 18,
+              paddingLeft: 20
+            }}
+          >
+            Lun, 8 de Junio.
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 18,
+              paddingLeft: 20,
 
-        <Button onPress={() => this.props.navigation.navigate('Reservation')}>
-          <Text>Aceptar</Text>
-        </Button>
+              paddingBottom: 10
+            }}
+          >
+            8:00 AM
+          </Text>
+        </Swipeable>
+        <Swipeable
+          leftButtonWidth={100}
+          rightButtonWidth={100}
+          rightButtons={rightButtons}
+          style={{
+            height: 100,
+            backgroundColor: 'gray'
+          }}
+        >
+          <Text
+            style={{
+              color: 'white',
+              fontWeight: 'bold',
+              fontSize: 20,
+              paddingLeft: 20,
+              paddingTop: 10
+            }}
+          >
+            Yolimar
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 18,
+              paddingLeft: 20
+            }}
+          >
+            Lun, 8 de Junio.
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 18,
+              paddingLeft: 20,
+
+              paddingBottom: 10
+            }}
+          >
+            8:00 AM
+          </Text>
+        </Swipeable>
       </View>
     );
   }
 }
-
-_reservation = () => {
-  this.props.navigation.navigate('Reservation');
-};
